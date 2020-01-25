93791ee8a05ac9affc01d499be9cc1a3cfa07d97
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-01-25 20:24:59 +0100

    Disallowed sending to prod on pull request

--- travis_build.sh
+++ travis_build.sh
@@ -1,15 +1,27 @@
 echo "========================="
+echo "Sending to production server"
+echo "========================="
+echo "Script made by: Piotr Stadnicki"
+echo "email: pitstadnicki@gmail.com"
+echo "github: fastfend"
+echo "========================="
 echo "Branch:"
 echo $TRAVIS_BRANCH
 echo "========================="
-if [[ $TRAVIS_BRANCH == "master" ]] || [[ $TRAVIS_BRANCH == "development" ]]
+if [[ $TRAVIS_PULL_REQUEST == false]]
 then
-    echo "Listing files:"
-    ls -la
-    echo "Packing files..."
-    cd dist/ && tar -zcvf ../build.tar.gz . && cd - 
-    echo "Sending files..."
-    jo key=$KEY branch=$TRAVIS_BRANCH build=%build.tar.gz | curl -H "Content-Type:application/json" -X POST -d @- $HOOK
+    if [[ $TRAVIS_BRANCH == "master" ]] || [[ $TRAVIS_BRANCH == "development" ]]
+    then
+        echo "Listing files:"
+        ls -la
+        echo "========================="
+        echo "Packing files..."
+        cd dist/ && tar -zcvf ../build.tar.gz . && cd - 
+        echo "Sending files..."
+        jo key=$KEY branch=$TRAVIS_BRANCH build=%build.tar.gz | curl -H "Content-Type:application/json" -X POST -d @- $HOOK
+    else
+        echo "Nothing to do..."
+    fi
 else
-    echo "Nothing to do..."
+echo "It is pull request doing nothing..."
 fi
