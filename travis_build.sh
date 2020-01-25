7c218cbd0fb931a91ab91eaee1605e146128cba4
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-01-25 20:02:46 +0100

    Added Travis CI

---
+++ travis_build.sh
@@ -0,0 +1,15 @@
+echo "========================="
+echo "Branch:"
+echo $TRAVIS_BRANCH
+echo "========================="
+if [[ $TRAVIS_BRANCH == "master" ]] || [[ $TRAVIS_BRANCH == "development" ]]
+then
+    echo "Listing files:"
+    ls -la
+    echo "Packing files..."
+    cd public/ && tar -zcvf ../build.tar.gz . && cd - 
+    echo "Sending files..."
+    jo key=$KEY branch=$TRAVIS_BRANCH build=%build.tar.gz | curl -H "Content-Type:application/json" -X POST -d @- $HOOK
+else
+    echo "Nothing to do..."
+fi
