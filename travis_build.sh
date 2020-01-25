3b3d2f7aaf13afcf543aad7f96d7571400fbfede
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-01-25 20:03:26 +0100

    Changed "public" to "dist"

--- travis_build.sh
+++ travis_build.sh
@@ -7,7 +7,7 @@ then
     echo "Listing files:"
     ls -la
     echo "Packing files..."
-    cd public/ && tar -zcvf ../build.tar.gz . && cd - 
+    cd dist/ && tar -zcvf ../build.tar.gz . && cd - 
     echo "Sending files..."
     jo key=$KEY branch=$TRAVIS_BRANCH build=%build.tar.gz | curl -H "Content-Type:application/json" -X POST -d @- $HOOK
 else
