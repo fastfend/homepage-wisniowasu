c41024c0ce9b5f02287b57a90e4735d61b3ccff9
Piotr Stadnicki <pitstadnicki@gmail.com>
2020-05-07 20:37:59 +0200

    More linting

--- src/js/global.js
+++ src/js/global.js
@@ -1,65 +1,73 @@
-import "../scss/main.scss";
-import "../scss/menu.scss";
+import '../scss/main.scss';
+import '../scss/menu.scss';
 
-import "progressive-image.js";
-import "lazysizes";
+import 'progressive-image.js';
+import 'lazysizes';
 
 var app = (function () {
   var body = null;
   var menu = null;
   var menuItems = null;
   var enableMenu = function enableMenu(element) {
-    if (!element.classList.contains("nav-active")) {
-      element.classList.add("nav-active");
-      window.sessionStorage.setItem("wisniowasu-menulastopened", true);
+    if (!element.classList.contains('nav-active')) {
+      element.classList.add('nav-active');
       window.sessionStorage.setItem(
-        "wisniowasu-menulasttimetopened",
-        Date.now()
+        'wisniowasu-menulastopened',
+        true,
+      );
+      window.sessionStorage.setItem(
+        'wisniowasu-menulasttimetopened',
+        Date.now(),
       );
     }
   };
   var disableMenu = function disableMenu(element) {
-    if (element.classList.contains("nav-active")) {
-      element.classList.remove("nav-active");
-      window.sessionStorage.setItem("wisniowasu-menulastopened", false);
+    if (element.classList.contains('nav-active')) {
+      element.classList.remove('nav-active');
+      window.sessionStorage.setItem(
+        'wisniowasu-menulastopened',
+        false,
+      );
     }
   };
   var toggleClass = function toggleClass(element) {
-    if (element.classList.contains("nav-active")) {
+    if (element.classList.contains('nav-active')) {
       disableMenu(element);
     } else {
       enableMenu(element);
     }
   };
   var applyListeners = function applyListeners() {
-    menu.addEventListener("click", function () {
-      var x = document.querySelectorAll(".hover");
+    menu.addEventListener('click', function () {
+      var x = document.querySelectorAll('.hover');
       if (x !== null) {
         x.forEach((element) => {
-          element.classList.remove("hover");
+          element.classList.remove('hover');
         });
       }
 
       return toggleClass(body);
     });
-    window.addEventListener("click", function (e) {
-      var x = document.getElementsByClassName("nav");
+    window.addEventListener('click', function (e) {
+      var x = document.getElementsByClassName('nav');
       if (!(x[0].contains(e.target) || menu.contains(e.target))) {
         return disableMenu(body);
       }
     });
   };
   var init = function init() {
-    body = document.querySelector("body");
-    menu = document.querySelector(".menu-icon");
-    menuItems = document.querySelectorAll(".nav__list-item");
+    body = document.querySelector('body');
+    menu = document.querySelector('.menu-icon');
+    menuItems = document.querySelectorAll('.nav__list-item');
     var opened = JSON.parse(
-      window.sessionStorage.getItem("wisniowasu-menulastopened")
+      window.sessionStorage.getItem('wisniowasu-menulastopened'),
     );
     var diff =
       Date.now() -
       JSON.parse(
-        window.sessionStorage.getItem("wisniowasu-menulasttimetopened")
+        window.sessionStorage.getItem(
+          'wisniowasu-menulasttimetopened',
+        ),
       );
     var seconds = Math.floor(diff / 1000);
     if (opened && seconds < 120) {
--- src/views/templates/projects/projects.js
+++ src/views/templates/projects/projects.js
@@ -1,34 +1,37 @@
-import "./projects.scss";
-import scrollreveal from "scrollreveal";
-import smoothscroll from "smoothscroll-polyfill";
+import './projects.scss';
+import scrollreveal from 'scrollreveal';
+import smoothscroll from 'smoothscroll-polyfill';
 
 smoothscroll.polyfill();
 
-document.body.classList.add("nologo");
+document.body.classList.add('nologo');
 window.onscroll = function () {
   var scrollPosition = document.scrollingElement.scrollTop;
   var body = document.body;
 
   if (scrollPosition > 170) {
-    body.classList.add("nav-bg");
+    body.classList.add('nav-bg');
   } else {
-    if (body.classList.contains("nav-bg")) {
-      body.classList.remove("nav-bg");
+    if (body.classList.contains('nav-bg')) {
+      body.classList.remove('nav-bg');
     }
   }
 };
 
-var menuItems = document.querySelectorAll(".img_container");
+var menuItems = document.querySelectorAll('.img_container');
 
-scrollreveal().reveal(document.querySelectorAll(".container"), {
-  easing: "ease-in-out",
-  distance: "20px",
+scrollreveal().reveal(document.querySelectorAll('.container'), {
+  easing: 'ease-in-out',
+  distance: '20px',
 });
 
 function checkOpen(element2) {
   var found = false;
   menuItems.forEach((element) => {
-    if (element.parentNode.classList.contains("open") && element !== element2) {
+    if (
+      element.parentNode.classList.contains('open') &&
+      element !== element2
+    ) {
       found = element;
     }
   });
@@ -39,23 +42,23 @@ menuItems.forEach((element) => {
   element.onclick = function () {
     var lastopenened = checkOpen(element);
     if (lastopenened !== false) {
-      lastopenened.parentNode.classList.remove("open");
+      lastopenened.parentNode.classList.remove('open');
     }
-    if (element.parentNode.classList.contains("open")) {
-      element.parentNode.classList.remove("open");
+    if (element.parentNode.classList.contains('open')) {
+      element.parentNode.classList.remove('open');
     } else {
-      element.parentNode.classList.add("open");
+      element.parentNode.classList.add('open');
       if (lastopenened !== false) {
         setTimeout(function () {
           element.parentNode.scrollIntoView({
-            block: "center",
-            behavior: "smooth",
+            block: 'center',
+            behavior: 'smooth',
           });
         }, 201);
       } else {
         element.parentNode.scrollIntoView({
-          block: "center",
-          behavior: "smooth",
+          block: 'center',
+          behavior: 'smooth',
         });
       }
     }
