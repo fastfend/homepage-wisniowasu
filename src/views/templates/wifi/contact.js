49aa06076bac168e56d8d6102292171da5a24a07
TerribleCsharp <68471853+TerribleCsharp@users.noreply.github.com>
2021-08-10 21:58:30 +0200

    Finished contact form

--- src/views/templates/wifi/contact.js
+++ src/views/templates/wifi/contact.js
@@ -21,29 +21,65 @@ window.onscroll = function () {
   }
 };
 
-var form = document.getElementById('form');
+const form = document.getElementById('form');
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('modal_overlay');
+const closeButton = document.getElementById('modal_close_btn');
 
 function sendMail(e) {
   e.preventDefault();
-  let form = {
-    name,
-    subject,
-    email,
-    message,
-  }
 
+  const form = {};
+  form.from = 'Formularz kontaktowy WiFI 2021';
   form.name = e.target.name.value;
   form.title = e.target.subject.value;
   form.email = e.target.email.value;
   form.message = e.target.message.value;
 
-  fetch("https://api.wisniowasu.pl/mailer/sendmail", {
-			method: "post",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(form),
-		});
+  fetch('https://api.wisniowasu.pl/mailer/sendmail', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(form),
+  })
+    .then((response) => response.text())
+    .then((data) => handleResponse(data));
+
+  overlay.classList.add('active');
+}
+
+function handleResponse(res) {
+  const iconSuccess = document.getElementById(
+    'js_modal_icon_success'
+  );
+  const iconFail = document.getElementById('js_modal_icon_fail');
+  const header = document.getElementById('js_modal_header');
+  const message = document.getElementById('js_modal_message');
+
+  console.log(res);
+  if (res === 'OK') {
+    iconSuccess.classList.add('active');
+    iconFail.classList.remove('active');
+    header.innerHTML = 'Poszło!';
+    message.innerHTML = 'Dzięki za kontakt! Odezwiemy się wkrótce ;)';
+  } else {
+    iconFail.classList.add('active');
+    iconSuccess.classList.remove('active');
+    header.innerHTML = 'Ups...';
+    message.innerHTML =
+      'Coś poszło nie tak. Spróbuj ponownie później';
+  }
+
+  modal.classList.add('active');
+  overlay.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+  overlay.removeEventListener('click', closeModal);
 }
 
 form.addEventListener('submit', sendMail);
+closeButton.addEventListener('click', closeModal);
